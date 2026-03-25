887392751a98ddb6cd81c448022cc93817498154
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-25 15:11:39 +0100

    reorganized the merged parts

--- generator.py
+++ generator.py
@@ -1,9 +1,22 @@
+import csv
 import random
 from typing import List, Tuple
-import csv
-#generate random number
+
+
+# generate random number
 def generateRandomnumbers():
-    return random.randint(1,10)
+    return random.randint(1, 10)
+
+
+# create 10 numbers
+numbers = [generateRandomnumbers() for i in range(10)]
+
+# write csv file
+with open("randomdata.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    for number in numbers:
+        writer.writerow([number])
+
 
 def create_coords(*, cols: int, rand_lower: int, rand_upper: int) -> List[Tuple[int, int, int]]:
     coords_list: List[Tuple[int, int, int]] = []
@@ -15,10 +28,9 @@ def create_coords(*, cols: int, rand_lower: int, rand_upper: int) -> List[Tuple[
         print(x, y, z)
         coords_list.append((x, y, z))
     return coords_list
-#create 10 numbers
-numbers=[generateRandomnumbers() for i in range(10)]
 
-def save_coords(coords:List[Tuple[int, int, int]], filename="data.csv"):
+
+def save_coords(coords: List[Tuple[int, int, int]], filename="data.csv"):
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(coords)  # writerows() writes the entire list at once
@@ -27,13 +39,3 @@ def save_coords(coords:List[Tuple[int, int, int]], filename="data.csv"):
 if __name__ == "__main__":
     coords = create_coords(cols=100, rand_lower=0, rand_upper=100)
     save_coords(coords)
-
-
-
-
-
-#write csv file
-with open("randomdata.csv",mode="w", newline="") as file:
-    writer=csv.writer(file)
-    for number in numbers:
-        writer.writerow([number])
