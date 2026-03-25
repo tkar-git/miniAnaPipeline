5a82a9c213bfcd8b4ea36220caa4b876b3b26302
janszlt <jan.stuetzle@icloud.com>
2026-03-25 15:45:21 +0100

    Removed Artefacts

--- analyze.py
+++ analyze.py
@@ -2,7 +2,6 @@ import csv
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-<<<<<<< HEAD
 def analyze_data(file_path):
     # Wir laden alle 4 Spalten
     data_dict = {'r': [], 'theta': [], 'phi': []}
@@ -58,28 +57,3 @@ for idx, group_keys in enumerate(groups):
 
 plt.tight_layout()
 plt.show()
-=======
-def analyze_data():
-    r, theta, phi = [], [], []
-
-    file_path = Path(__file__).parent / "data.csv"
-
-    with open(file_path) as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  #Header skip
-
-        for row in reader:
-            r.append(float(row[0]))
-            theta.append(float(row[1]))
-            phi.append(float(row[2]))
-
-    plt.figure(figsize=(8, 5))
-    plt.boxplot([r, theta, phi], tick_labels=["r", "theta", "phi"])
-    plt.title("Boxplot der Koordinaten")
-    plt.ylabel("Wert")
-    plt.grid(axis="y", linestyle="--", alpha=0.3)
-    plt.tight_layout()
-    plt.show()
-
-analyze_data()
->>>>>>> db2a398d4104af748b9b094cbfb7726d09f17130
