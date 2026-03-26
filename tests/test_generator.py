e9967b2d516ba2d21e3262c37ac0f6b3ba7ce70b
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-26 14:29:08 +0100

    added test cases for the datagenerator

---
+++ tests/test_generator.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import pytest
+import analyze
+
+def test_create_coords():
+    len = size
+    lower_limit = 0
+    upper_limit = 100
+    coords = analyze.create_coords(cols=size, rand_lower=lower_limit, rand_upper=upper_limit)
+    assert len(coords) == size
+    assert all(isinstance(coord, tuple) for coord in coords)
+    assert all(len(coord) == 3 for coord in coords)
+    assert all(isinstance(x, int) for x, _, _ in coords)
+    assert all(isinstance(y, int) for _, y, _ in coords)
+    assert all(isinstance(z, int) for _, _, z in coords)
+    assert all(lower_limit <= x <= upper_limit for x, _, _ in coords)
+    assert all(lower_limit <= y <= upper_limit for _, y, _ in coords)
+    assert all(lower_limit <= z <= upper_limit for _, _, z in coords)
+
+if __name__ == "__main__":
+    pytest.main()
+
