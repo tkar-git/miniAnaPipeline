645064bead498372166cd96f58506ada6d20e7d8
Tamasi Kar <kar@physi.uni-heidelberg.de>
2026-03-26 10:59:33 +0100

    Revert "Revert "Merge branch 'datagenerator' of github.com:tkar-git/miniAnaPipeline into feature/readme""
    This reverts commit 522286f364b25751664e4a0a6915eb7c7a24c0ec.

--- generator.py
+++ generator.py
@@ -3,14 +3,15 @@ import random
 from typing import List, Tuple
 
 
-def create_coords(*, cols: int, rand_lower: int, rand_upper: int) -> List[Tuple[int, int, int]]:
+def create_coords(*, cols: int, rand_lower: int, rand_upper: int, verbose = False) -> List[Tuple[int, int, int]]:
     coords_list: List[Tuple[int, int, int]] = []
     for i in range(cols):
         # create coords
         x = random.randint(rand_lower, rand_upper)
         y = random.randint(rand_lower, rand_upper)
         z = random.randint(rand_lower, rand_upper)
-        print(x, y, z)
+        if verbose:
+            print(x, y, z)
         coords_list.append((x, y, z))
     return coords_list
 
