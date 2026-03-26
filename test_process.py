9e65065f5155fa2636cefeca4d046ef6d6b97700
jul-sy <j-huebi@mailbox.org>
2026-03-26 15:03:06 +0100

    tried to assert header line

--- test_process.py
+++ test_process.py
@@ -4,3 +4,12 @@ import csv
 
 # asserts if required data.csv file exists
 assert path.exists("data.csv"), "data.csv file does not exists"
+
+'''
+#opens data.csv if exists and reads out rows with csv.reader
+file = open(path.exists("data.csv"), newline='')
+reader=csv.reader(file)
+header=next(reader) #first line of csv file
+assert header=='x,y,z', "header is not accurate"
+'''
+# doesnt work as intended yet
